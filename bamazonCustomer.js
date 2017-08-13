19524affd3ab86808856f01c78be13ff6ae5f034
nwdavis <nathanieldavis89@gmail.com>
2017-08-13 18:53:14 -0400

    mysql calls working, almost done with customer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,90 @@
-var db = require('mysql');
+var mysql = require('mysql');
 var inquirer = require('inquirer');
+
+//creating mysql connection
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "1234",
+    database: "bamazon"
+  });
+
+//connection error handling and app start
+connection.connect(function(err) {
+    if (err) throw err;
+    start();
+  });
+
+function start(){
+    connection.query("SELECT * FROM products", function(err, res){
+        if (err) throw err;
+
+        for (i = 0; i < res.length; i++){
+            
+            console.log("-----------------");
+            console.log("ID: " + res[i].item_id);
+            console.log(res[i].product_name);
+            console.log("$"+ res[i].price);
+            console.log("-----------------");
+        }
+        
+
+        inquirer.prompt([
+            {
+                name: "itemChoice",
+                type: "input",
+                message: "Enter ID of the item you want to purchase.",
+                filter: function(str){
+                    return parseInt(str)
+                },
+                validate: function(str){
+                    //adding validation for entry
+                    if (str < 1 || str > res.length || isNaN(str)){
+                        console.log("\nEnter a valid product ID.")
+                    }else{
+                        return true
+                    }
+                }
+            },
+            {
+                name: "itemQuant",
+                type: "input",
+                message: "How many would you like to buy?",
+                filter: function(str){
+                    return parseInt(str)
+                },
+                validate: function(str){
+                    //adding validation for entry
+                    if (str < 1 || isNaN(str)){
+                        console.log("\nEnter a valid quantity.")
+                    }else{
+                        return true
+                    }
+                }
+            },
+            {
+                name: "confirm",
+                type: "confirm",
+                message: `Place Order?`
+            }
+
+        ]).then(function(answer){
+
+            inventoryCheck(answer.itemChoice - 1, answer.itemQuant);
+            
+        });
+
+        function inventoryCheck(item, quantity){
+            if (res[item].stock_quantity < quantity){
+                console.log(`Sorry, we don't have enough of ${res[item].product_name}.`)
+            } else {
+                console.log(`${res[item].product_name} has been purchased.`)
+            }
+        }
+
+
+    });
+
+}
+
